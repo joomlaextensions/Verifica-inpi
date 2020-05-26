02de1b73601cac097c4027c0f1c28bb79b09c526
Karine Pires <pires.karine6@gmail.com>
2020-05-26 11:12:41 -0300

    Ajustes na implementacao

--- models/verificainpi.php
+++ models/verificainpi.php
@@ -8,8 +8,6 @@
  * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
-
-
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
@@ -20,7 +18,9 @@ use Joomla\Registry\Registry;
 jimport('joomla.application.component.model');
 
 require_once JPATH_SITE . '/plugins/fabrik_cron/verificainpi/models/registro.php';
+
 require_once JPATH_SITE . '/plugins/fabrik_cron/verificainpi/models/alerta.php';
+
 
 /**
  * The cron verifica inpi plugin model.
@@ -29,29 +29,18 @@ require_once JPATH_SITE . '/plugins/fabrik_cron/verificainpi/models/alerta.php';
  * @subpackage  Fabrik.cron.notification
  * @since       3.0
  */
-
-
-
 class FabrikModelVerificaInpi extends FabModel {
 
 	protected $nomes_arq;
-
 	protected $nomes_arq_varredura;
-
   protected $status_download;
-
 	protected $status_unzip;
-
 	private $xml, $pro_tag, $revista, $data, $sufixo, $varridos, $secao;
-
 	protected $params;
-
 	protected $table_alerta;
-
 	protected $table_patente;
-
 	protected $table_inpi;
-
+	protected $log;
 
 
 	/**
@@ -61,9 +50,6 @@ class FabrikModelVerificaInpi extends FabModel {
 	 *
 	 * @since   11.1
 	 */
-
-
-
 	public function __construct($config = array())
 	{
 		$this->params = $config["params"];
@@ -72,11 +58,11 @@ class FabrikModelVerificaInpi extends FabModel {
 		$idInpi = $this->params->get('table_verificainpi_inpi');
 		$idPatente = $this->params->get('table_verifica');
 
-		$this->table_alerta = $this->getTable($idAlerta);
+		$this->log = '';
 
-		$this->table_inpi = $this->getTable($idInpi);
-
-		$this->table_patente = $this->getTable($idPatente);
+		$this->table_alerta = $this->getTable(null, null, array($idAlerta));
+		$this->table_inpi = $this->getTable(null, null, array($idInpi));
+		$this->table_patente = $this->getTable(null, null, array($idPatente));
 
 		parent::__construct($config);
 	}
@@ -85,10 +71,14 @@ class FabrikModelVerificaInpi extends FabModel {
 		return $this->status_download;
   }
 
+
   public function statusUnzip() {
   	return $this->status_unzip;
-  }
-
+	}
+	
+	public function getLog() {
+  	return $this->log;
+	}
 
 	/**
 	 * Get Rows from Revista
@@ -116,23 +106,23 @@ class FabrikModelVerificaInpi extends FabModel {
 	 *
 	 * @return   object	table
 	 */
-	public function getTable($idList)
+	public function getTable($name = '', $prefix = 'Table', $options = array())
 	{
 		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_fabrik/tables');
 		$this->table = FabTable::getInstance('List', 'FabrikTable');
 
-		if ($idList !== 0)
-		{
-			$this->table->load($idList);
+		if ($options[0] !== 0){
+			$this->table->load($options[0]);
 		}
 
-		if (trim($this->table->db_primary_key) !== '')
-		{
+		if (trim($this->table->db_primary_key) !== ''){
 			$this->table->db_primary_key = FabrikString::safeColName($this->table->db_primary_key);
 		}
 
 		return $this->table->db_table_name;
 	}
+
+
 
 	public function downloadRevista($revista){
 		$this->nomes_arq[0] = 'http://revistas.inpi.gov.br/txt/CT'.$revista.'.zip';
@@ -159,46 +149,49 @@ class FabrikModelVerificaInpi extends FabModel {
 					$destino = getcwd()."/tmp/Zip//RM".$revista.'.zip';	//Destino da descompactação
 					break;
 				default:
-					echo "Arquivo destino não encontrado.<br>";
+					$this->log .=  "Arquivo destino não encontrado.<br>";
 					break;
 			}
 
 			$headers = @get_headers($url);
-
 			if(strpos($headers[1],'404') === false)		//Verifica se URL é valida para downloads.
-			{
-				echo "URL Exists<br>";													
-				$ch = curl_init($url);										
-				$fp = fopen($destino, "wb");
-
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_FILE, $fp);
-				curl_setopt($ch, CURLOPT_HEADER, 0);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-				$saida = curl_exec($ch);
-
-				if ($saida) {
-				    echo "Download realizado!<br>";
-				}
-				else {
-				    echo "Erro no download<br>";
-				    $this->status_download = false;
+			{													
+				$ch = curl_init($url);
+				
+				if (!is_dir(getcwd()."/tmp/Zip/")) {
+					mkdir(getcwd()."/tmp/Zip/", 0755, true);
 				}
 
-				fclose($fp);
-				curl_close($ch);
-			}
-			else
-			{
-			  echo "URL Not Exists<br>";
+				if(file_exists($destino)){
+					$this->log .= "File in " . $destino . "already exists <br>"; 
+					$this->status_download = true;
+				} else {
+					$fp = fopen($destino, "wb");
 
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_FILE, $fp);
+					curl_setopt($ch, CURLOPT_HEADER, 0);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+					curl_setopt($ch, CURLOPT_ENCODING, "");
+
+					$saida = curl_exec($ch);
+					if ($saida) {
+						$this->log .= "Download da revista " . $revista . '.zip' . "realizado!<br>";
+					}	else {
+						$this->log .= "Erro no download da revista " . $revista . '.zip' . "<br>";
+						$this->status_download = false;
+					}
+
+					fclose($fp);
+					curl_close($ch);
+				}
+			} else {
+				$this->log .= "URL Not Exists<br>";
 			  $this->status_download = false;
 			  return;
-			}			
-
+			}
 		}
 
 		$this->status_download = true;
@@ -212,51 +205,50 @@ class FabrikModelVerificaInpi extends FabModel {
 		$this->nomes_arq[3] = 'PC'.$revista.'.zip';
 		$this->nomes_arq[4] = 'RM'.$revista.'.zip';
 
-
 		foreach ($this->nomes_arq as $key => $value) {
 			$arquivo = getcwd()."/tmp/Zip//".$value;	//Local do arquivo .zip
 			$destino = getcwd()."/tmp/Arquivos//";	//Destino da descompactação
+
 			$zip = new \ZipArchive;
 			$zip->open($arquivo);
 
 			if($zip->extractTo($destino) == TRUE){
-				echo "Arquivo descompactado com sucesso.<br>";
+				$this->log .= "Arquivo descompactado com sucesso.<br>";
 			} else {
-				echo "O Arquivo não pode ser descompactado.<br>";
+				$this->log .= "O Arquivo não pode ser descompactado.<br>";
 				$this->status_unzip = false;
 				return;
 			}
-
 			$zip->close();
-
 		}
 
 		$this->status_unzip = true;
-
   }
 
 	public function getCodigoPatente(){
 		$attr_cod = preg_split("/___/", $this->params->get('element_table_verifica'));
-  	$db = FabrikWorker::getDbo();
+		
+		$db = FabrikWorker::getDbo();
 		$query = $db->getQuery(true);
 		$query->select($attr_cod[1])->from($this->table_patente);
 		$db->setQuery($query);
+
 		return $db->loadObjectList();
 	}
 
-
 	public function varrer($revista, $data, $cods){
+
 		$this->revista = $revista;
 		$this->data = $data;
 		$this->sufixo = $revista.'_'.$data.'.xml';
+
 		$this->nomes_arq_varredura[0] = 'Patente_';
 		$this->nomes_arq_varredura[1] = 'DesenhoIndustrial_';
 		$this->nomes_arq_varredura[2] = 'Contratos_';
 		$this->nomes_arq_varredura[3] = 'Programa_';
 
-
 		foreach ($this->nomes_arq_varredura as $key => $value) {
-			echo "VARRENDO SEÇÃO $value<br><hr>";
+			$this->log .= "<br>VARRENDO SEÇÃO $value<br><hr>";
 
 			/*
 			if($key == 4){
@@ -267,58 +259,57 @@ class FabrikModelVerificaInpi extends FabModel {
 			}*/
 
 			$local = getcwd() . '/tmp/Arquivos/' . $value . $this->sufixo;
+			if (file_exists($local)) {
+				if($key == 0){
+					$this->pro_tag = 'processo-patente';	//Tag's de controle do .xml de cada seção
+					$this->secao = 'Secao VI';
+				} elseif ($key == 1) {
+					$this->pro_tag = 'processo-patente';
+					$this->secao = 'Secao III';
+				} elseif($key == 2){
+					$this->pro_tag = 'processo-contrato';
+					$this->secao = 'Secao II';
+				} elseif ($key == 3) {
+					$this->pro_tag = 'processo-programa';
+					$this->secao = 'Secao VII';
+				}
 
-			if($key == 0){
-				$this->pro_tag = 'processo-patente';	//Tag's de controle do .xml de cada seção
-				$this->secao = 'Secao VI';
+				$this->compare($cods, $local);
+				$this->log .= "<hr>";
+				$this->gerarAlertas();
+				unset($this->varridos);
+			} else {
+				$this->log .= "The file $local does not exist<br>";
 			}
-			elseif ($key == 1) {
-				$this->pro_tag = 'processo-patente';
-				$this->secao = 'Secao III';
-			}
-			elseif($key == 2){
-				$this->pro_tag = 'processo-contrato';
-				$this->secao = 'Secao II';
-			}
-			elseif ($key == 3) {
-				$this->pro_tag = 'processo-programa';
-				$this->secao = 'Secao VII';
-			}
-
-			$this->compare($cods, $local);
-			echo "<hr>";
-			$this->gerarAlertas();
-			unset($this->varridos);
 		}
-
 	}
 
 	public function compare($cods, $local){
 		$this->xml = simplexml_load_file($local); //carrega o arquivo XML e retornando um Array
 		$id = 0;
 
-		foreach ($this->xml->despacho as $des) {	//Loop percorre todo o arquivo xml, captando cada tag 'despacho'
-
-			foreach ($cods as $value) {
-				if((string)$des->{$this->pro_tag}->numero === $value->codigo_do_pedido){		//Compara códigos do BD com os códigos submetidos na revista.
-					$this->varridos[$id]['revista'] = $this->revista;
-					$this->varridos[$id]['secao'] = $this->secao;
-					$this->varridos[$id]['codigo'] = $des->codigo;
-					$this->varridos[$id]['titulo'] = $des->titulo;
-					$this->varridos[$id]['numero'] = (string) $des->{$this->pro_tag}->numero;
-
-					if(isset($des->{$this->pro_tag}->titulo)){
-						$this->varridos[$id]['projeto'] = (string) $des->{$this->pro_tag}->titulo;
-					}else{
-						$this->varridos[$id]['projeto'] = '--';
+		if (is_array($this->xml->despacho) || is_object($this->xml->despacho)){
+			foreach ($this->xml->despacho as $des) {	//Loop percorre todo o arquivo xml, captando cada tag 'despacho'
+				foreach ($cods as $value) {
+					if((string)$des->{$this->pro_tag}->numero === $value->codigo_do_pedido){		//Compara códigos do BD com os códigos submetidos na revista.
+						$this->varridos[$id]['revista'] = $this->revista;
+						$this->varridos[$id]['secao'] = $this->secao;
+						$this->varridos[$id]['codigo'] = $des->codigo;
+						$this->varridos[$id]['titulo'] = $des->titulo;
+						$this->varridos[$id]['numero'] = (string) $des->{$this->pro_tag}->numero;
+	
+	
+						if(isset($des->{$this->pro_tag}->titulo)){
+							$this->varridos[$id]['projeto'] = (string) $des->{$this->pro_tag}->titulo;
+						}else{
+							$this->varridos[$id]['projeto'] = '--';
+						}
+	
+						$id++;
+						$this->log .= "<br><br>";
+						$this->log .= 'Adicionado '.$id.'. Codigo : '.(string) $des->{$this->pro_tag}->numero.' Titulo : '.$des->titulo."<br>";
+						break;
 					}
-
-					$id++;
-
-
-					echo "<br><br>";
-					echo 'Adicionado '.$id.'. Codigo : '.(string) $des->{$this->pro_tag}->numero.' Titulo : '.$des->titulo."<br>";
-					break;
 				}
 			}
 		}
@@ -331,15 +322,10 @@ class FabrikModelVerificaInpi extends FabModel {
 	}
 
 	public function createRegistro(Registro $r){	//Cria novo registro na TABLE_VERIFICA.
-	
 		$attr_revista = preg_split("/___/", $this->params->get('element_verificainpi_inpi_revista'));
-
 		$attr_secao = preg_split("/___/", $this->params->get('element_verificainpi_inpi_secao'));
-
 		$attr_patentes_citadas = preg_split("/___/", $this->params->get('element_verificainpi_inpi_patentes_citadas'));
-
 		$attr_patentes_n_citadas = preg_split("/___/", $this->params->get('element_verificainpi_inpi_patentes_n_citadas'));
-
 		$attr_data = preg_split("/___/", $this->params->get('element_verificainpi_inpi_data'));
 
 		// Get a db connection.
@@ -354,32 +340,29 @@ class FabrikModelVerificaInpi extends FabModel {
 		// Insert values.
 		$values = array('CURRENT_TIMESTAMP()', $r->getRevista(), $db->quote($r->getSecao()), $db->quote($r->getPatentesCitadas()), $db->quote($r->getPatentes_N_Citadas()));
 
-
 		// Prepare the insert query.
 		$query
 				->insert($db->quoteName($this->table_inpi))
 				->columns($db->quoteName($columns))
 				->values(implode(',', $values));
 
-
 		// Set the query using our newly populated query object and execute it.
 		$db->setQuery($query);
-
 		$db->execute();
 
-		echo "Registro Inserido com exito !<br>";
+		$this->log .= "Registro Inserido com exito !<br>";
 	}
-
 
 	public function gerarAlertas(){
 		if(!array_key_exists('codigo', $this->varridos[0])){
-			echo "Nenhuma patente foi citada na revista ".$this->revista."<br>";
-		}
-		else{
+			$this->log .= "Nenhuma patente foi citada na revista ".$this->revista."<br>";
+		} else {
 			$alerta = new Alerta();
+
 			foreach ($this->varridos as $id => $value) {	//Preenche descrição de cada alerta
 				$desc = 'O pedido '.$value['numero'].' (Projeto : '.$value['projeto'].') foi mencionado na revista '.$value['revista'].', na '.$this->secao.'. No intuito : '.$value['titulo'].' ('.$value['codigo'].')';
-				echo "<br><br>";
+				
+				$this->log .= "<br><br>";
 
 				$alerta->setDescricao($desc);
 				$alerta->setStatus('Nao tratado');
@@ -390,17 +373,14 @@ class FabrikModelVerificaInpi extends FabModel {
 
 		$cods = $this->getCodigoPatente();
 		$verifica = new Registro($cods, $this->varridos);
-
 		$this->createRegistro($verifica);
 	}
-
-
 
 	public function createAlerta(Alerta $a){	//Cria novo alerta na tabela TABLE_ALERTA.
 		$attr_descricao = preg_split("/___/", $this->params->get('element_verificainpi_alerta_desc'));
 		$attr_situacao = preg_split("/___/", $this->params->get('element_verificainpi_alerta_status'));
 		$attr_data = preg_split("/___/", $this->params->get('element_verificainpi_alerta_data'));
-		
+
 		// Get a db connection.
 		$db = JFactory::getDbo();
 
@@ -413,7 +393,6 @@ class FabrikModelVerificaInpi extends FabModel {
 		// Insert values.
 		$values = array($db->quote($a->getDescricao()), $db->quote($a->getStatus()), 'CURRENT_TIMESTAMP()');
 
-
 		// Prepare the insert query.
 		$query
 				->insert($db->quoteName($this->table_alerta))
@@ -423,11 +402,9 @@ class FabrikModelVerificaInpi extends FabModel {
 
 		// Set the query using our newly populated query object and execute it.
 		$db->setQuery($query);
-
 		$db->execute();
 
-		echo "Alerta Inserido com exito !<br>";
+		$this->log .= "Alerta Inserido com exito !<br>";
 	}
-
 
 }
--- verificainpi.php
+++ verificainpi.php
@@ -17,9 +17,6 @@ use Joomla\Utilities\ArrayHelper;
 // Require the abstract plugin class
 require_once COM_FABRIK_FRONTEND . '/models/plugin-cron.php';
 
-//require_once JPATH_SITE . '/plugins/fabrik_cron/verificainpi/models/verificainpi.php';
-
-
 /**
  * 
  *
@@ -27,8 +24,8 @@ require_once COM_FABRIK_FRONTEND . '/models/plugin-cron.php';
  * @subpackage  Fabrik.cron.verificainpi
  * @since       1.0
  */
-class PlgFabrik_CronVerificaInpi extends PlgFabrik_Cron
-{
+
+class PlgFabrik_CronVerificaInpi extends PlgFabrik_Cron {
 
 	/**
 	 * Do the plugin action
@@ -45,27 +42,28 @@ class PlgFabrik_CronVerificaInpi extends PlgFabrik_Cron
 		$model = JModelLegacy::getInstance('VerificaInpi', 'FabrikModel', array('ignore_request' => true, 'params' => $params));
 		$ult_revista = (int) $model->getRevista()[0]->revista; //Obtém número da última revista verificada
 		$revista = isset($ult_revista) ? $ult_revista + 1 : '';	//Incrementa número da revista para a próxima ocorrênica 
-
 		$data = date('dmY');	//Obtém a data atual
+		$logMsg = '';
 
 		if(isset($revista) && !empty($revista)){
-			$model->downloadRevista($revista); //Realiza downloads das seções da revista
+			$model->downloadRevista($revista); //Realiza downloads das seções da revist
 			if($model->statusDownload()){		//Verifica se downloads ocorreram com exito
 				$model->descompactaRevista($revista);	//Descompacta as pastas .zip 
 				if($model->statusUnzip()){
 					$cods = $model->getCodigoPatente();		//Obtem todos os dados do campo 'codigo' da tabela patentes.
 					$model->varrer($revista, $data, $cods);	//Realiza varredura, e efetua os alertas e registros no BD.
+				} else {
+					$logMsg .= "Arquivos corrompidos.<br>";	//Erro na descompactação dos arquivos.
 				}
-				else{
-					echo "Arquivos corrompidos.<br>";	//Erro na descompactação dos arquivos.
-				}
-			}
-			else{
-				$this->log .= 'Revista não encontrada.<br />';
+			} else {
+				$logMsg .= 'Revista numero ' . $revista . ' não encontrada.<br />';
 			}
 		} else {
-			$this->log .= 'Não foi possível obter o número da revista.<br />';
+			$logMsg .= 'Não foi possível obter o número da revista.<br />';
 		}
+
+		$logMsg .= $model->getLog();
+		FabrikWorker::log('plg.cron.verificainpi.information', $logMsg);
 
 	}
 
